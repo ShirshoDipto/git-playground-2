3f95035b4b4f344e9a7d4b34b3efd88a4d4cfaa9
shirshodipto <shirshodipto@gmail.com>
2024-04-02 17:26:35 +0600

    Update main.py from editor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     print("Hello World from main!!")
     print("Edited this file in the test asdsad fetches second time.")
+    print("I just made some new commits")
+    print("I just made some new commits 2")
 
 
 if __name__ == "__main__":
